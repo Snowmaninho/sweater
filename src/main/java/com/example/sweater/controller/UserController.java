c1c829347db97576b82ad0ec68c6b68ae05ceb02
Snowman <icemaninho@yandex.ru>
2020-10-19 18:55:08 +0300

    DB migration and Profile

--- src/main/java/com/example/sweater/controller/UserController.java
+++ src/main/java/com/example/sweater/controller/UserController.java
@@ -2,33 +2,32 @@ package com.example.sweater.controller;
 
 import com.example.sweater.domain.Role;
 import com.example.sweater.domain.User;
-import com.example.sweater.repos.UserRepo;
+import com.example.sweater.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user") // Вместо указания мэппинга у каждого метода можно вынести сюда, если у всех методов он общий
-@PreAuthorize("hasAuthority('ADMIN')") // Перед каждым методом проверяется, является ли вользователь ADMIN
 public class UserController {
     @Autowired
-    UserRepo userRepo;
+    private UserService userService;
 
+    @PreAuthorize("hasAuthority('ADMIN')")  // Перед каждым методом проверяется, является ли вользователь ADMIN
     @GetMapping
     public String userList(Model model) {
-        model.addAttribute("users", userRepo.findAll());
+        model.addAttribute("users", userService.findAll());
 
         return "userList";
     }
 
-    @GetMapping("{user}") //помимо "/user" в адресе будет так же индификатор каждого пользователя
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @GetMapping("{user}") //помимо "/user" в адресе будет так же индификатор каждого пользователя через /
     public String userEditForm(@PathVariable User user, Model model) {
         model.addAttribute("user", user);
         model.addAttribute("roles", Role.values());
@@ -36,29 +35,34 @@ public class UserController {
         return "userEdit";
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PostMapping
     public String userSave(
             @RequestParam String username,
             @RequestParam Map<String, String> form,
             @RequestParam("userId") User user) {
 
-        user.setUsername(username);
-
-        Set<String> roles = Arrays.stream(Role.values())
-                                  .map(Role::name)
-                                  .collect(Collectors.toSet());
-
-        user.getRoles().clear();
-
-        for (String key : form.keySet()) {
-            if (roles.contains(key)) {
-                user.getRoles().add(Role.valueOf(key));
-            }
-        }
-
-        userRepo.save(user);
+        userService.saveUser(user, username, form);
 
         return "redirect:/user";
     }
 
+    @GetMapping("profile") //после "/user" в адресе будет так же "/profile"
+    public String getProfile(Model model, @AuthenticationPrincipal User user) {
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
+
+        return "profile";
+    }
+
+    @PostMapping("profile")
+    public String updateProfile(
+            @AuthenticationPrincipal User user,
+            @RequestParam String password,
+            @RequestParam String email
+    ) {
+        userService.updateProfile(user, password, email);
+
+        return "redirect:/user/profile";
+    }
 }
